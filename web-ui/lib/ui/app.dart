1ce7849eaf28e4214b6040351ab48ebf0fcc029f
elenaferr0 <elena.ferro@gmail.com>
2025-08-04 23:07:04 +0200

    feat: added searchbar

--- web-ui/lib/ui/app.dart
+++ web-ui/lib/ui/app.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart'
-    hide Scaffold, AppBar, ThemeData, Theme, Card;
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 import 'package:webui/repository/operation.dart';
 import 'package:webui/ui/widgets/operation.dart';
+import 'package:webui/ui/widgets/search_bar.dart';
 
 class App extends StatelessWidget {
+
+  static const _padding = 16.0;
   const App({super.key});
 
   @override
@@ -13,42 +14,55 @@ class App extends StatelessWidget {
       JsonOperation(
         name: 'Merge',
         description: 'Merge files together',
-        code: JsonOperationCode.merge
+        code: JsonOperationCode.merge,
       ),
       JsonOperation(
-          name: 'Split',
-          description: 'Split a file into multiple parts',
-          code: JsonOperationCode.split
+        name: 'Split',
+        description: 'Split a file into multiple parts',
+        code: JsonOperationCode.split,
       ),
       JsonOperation(
-          name: 'Merge',
-          description: 'Merge files together',
-          code: JsonOperationCode.merge
+        name: 'Merge',
+        description: 'Merge files together',
+        code: JsonOperationCode.merge,
       ),
       JsonOperation(
-          name: 'Merge',
-          description: 'Merge files together',
-          code: JsonOperationCode.merge
+        name: 'Merge',
+        description: 'Merge files together',
+        code: JsonOperationCode.merge,
       ),
     ];
+    
 
     return ShadcnApp(
       debugShowCheckedModeBanner: false,
       title: 'GonvertIt',
       home: Scaffold(
         headers: [AppBar(title: const Text('GonvertIt'))],
-        child: Center(
-          child: GridView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 400, // Maximum width per card
-              mainAxisSpacing: 16,
-              crossAxisSpacing: 16,
-              mainAxisExtent: 80, // Fixed height for each card
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(_padding),
+                child: SearchBar(),
+              ),
             ),
-            itemCount: ops.length,
-            itemBuilder: (context, index) => Operation(op: ops[index]),
-          ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: _padding),
+              sliver: SliverGrid(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 400, // Maximum width per card
+                  mainAxisSpacing: _padding,
+                  crossAxisSpacing: _padding,
+                  mainAxisExtent: 80, // Fixed height for each card
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => Operation(op: ops[index]),
+                  childCount: ops.length,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       theme: ThemeData(colorScheme: ColorSchemes.darkZinc(), radius: 0.5),
