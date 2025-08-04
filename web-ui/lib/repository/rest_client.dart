fc1c411565ec6f063d668cec6c3099ce39e3de2e
elenaferr0 <elena.ferro@gmail.com>
2025-08-04 23:29:40 +0200

    feat: configured injectable

--- web-ui/lib/repository/rest_client.dart
+++ web-ui/lib/repository/rest_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/retrofit.dart';
 
@@ -6,6 +7,7 @@ part 'rest_client.g.dart';
 
 @RestApi(baseUrl: 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/')
 abstract class RestClient {
+  @factoryMethod
   factory RestClient(Dio dio, {String? baseUrl}) = _RestClient;
 
   @GET('/tasks')
